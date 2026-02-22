9084370901add21abc8b1558144193c3f57d2556
Mariusz <rexxarhell@gmail.com>
2026-02-22 17:35:18 +0100

    chore: moved methods to hex

--- Domain/Hex/Hex.cs
+++ Domain/Hex/Hex.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TBS.Domain.Hex;
 
 public readonly record struct Hex(int Q, int R)
 {
+    private static readonly (int dq, int dr)[] Directions =
+    [
+        ( 1,  0),
+        (-1,  0),
+        ( 0,  1),
+        ( 0, -1),
+        ( 1, -1),
+        (-1,  1)
+    ];
+
+    public IEnumerable<Hex> Neighbours()
+    {
+        foreach (var (dq, dr) in Directions)
+            yield return new Hex(Q + dq, R + dr);
+    }
+    
     public int DistanceTo(Hex other)
     {
         int dq = Q - other.Q;
